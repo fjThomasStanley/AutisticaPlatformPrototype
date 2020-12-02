a605ebe94c71936763246c6b195ed99d6bab04f2
pgc007 <64270473+pgc007@users.noreply.github.com>
2020-12-02 18:20:38 +0000

    progress on changes to modal.html

--- static/scripts/components/modal.js
+++ static/scripts/components/modal.js
@@ -1,7 +1,21 @@
 var citizenSciencePlatform_modalfunctions = {
   ShowModal: function (modalID) {
     document.getElementById(modalID).style.display = "block";
-  },
+    var height = document.getElementById(showImageUpload).getAttribute('data-height');
+    var width = document.getElementById(showImageUpload).getAttribute('data-width');
+    if (height == "")
+    {
+        height = "250";
+    }
+    if (width == "")
+    {
+        width = "300";
+    }
+    document.documentElement.style.setProperty("--md-height") = height + "px";
+    document.documentElement.style.setProperty("--md-width") = width + "px";
+    document.documentElement.style.setProperty("--md-top") = "-" + toString(parseInt(height)/2) + "px";
+    document.documentElement.style.setProperty("--md-left") = "-" + toString(parseInt(width)/2) + "px";
+  },  },
 
   HideModal: function (modalID) {
     document.getElementById(modalID).style.display = "none";
