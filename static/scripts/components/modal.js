ec584536a7dbbd2767dae2ea56795b1849f069a9
pgc007 <64270473+pgc007@users.noreply.github.com>
2020-12-02 22:47:27 +0000

    fix using variables in showmodal

--- static/scripts/components/modal.js
+++ static/scripts/components/modal.js
@@ -1,21 +1,25 @@
 var citizenSciencePlatform_modalfunctions = {
   ShowModal: function (modalID) {
     document.getElementById(modalID).style.display = "block";
-    var height = document.getElementById(showImageUpload).getAttribute('data-height');
-    var width = document.getElementById(showImageUpload).getAttribute('data-width');
+
+    var height = document.getElementById("showImageUpload").getAttribute("data-height");
+
+    var width = document.getElementById("showImageUpload").getAttribute('data-width');
     if (height == "")
     {
         height = "250";
     }
+
     if (width == "")
     {
         width = "300";
     }
-    document.documentElement.style.setProperty("--md-height") = height + "px";
-    document.documentElement.style.setProperty("--md-width") = width + "px";
-    document.documentElement.style.setProperty("--md-top") = "-" + toString(parseInt(height)/2) + "px";
-    document.documentElement.style.setProperty("--md-left") = "-" + toString(parseInt(width)/2) + "px";
-  },  },
+
+    document.documentElement.style.setProperty("--md-height", height + "px");
+    document.documentElement.style.setProperty("--md-width", width + "px");
+    document.documentElement.style.setProperty("--md-top", "-" + (parseInt(height) / 2).toString() + "px");
+    document.documentElement.style.setProperty("--md-left", "-" + (parseInt(width) / 2).toString() + "px");
+  },
 
   HideModal: function (modalID) {
     document.getElementById(modalID).style.display = "none";
